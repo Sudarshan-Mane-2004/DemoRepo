0a6a25fc0c9070bae9a17a2a00c99bd82e3898db
Sudarshan Sidram Mane <160010956+Sudarshan-Mane-2004@users.noreply.github.com>
2025-12-26 19:41:26 +0530

    Update greeting message in main method

--- myfile1.java
+++ myfile1.java
@@ -1,5 +1,5 @@
 public class myfile1 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! welcome to jenkins");
     }
 }
